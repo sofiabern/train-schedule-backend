ca0f22796378693ca545754383a94f77482c0408
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2026-02-08 18:18:23 +0200

    fix: correct start path for Render (dist/src/main.js)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', 'bootstrap.js'],
+    ignores: ['eslint.config.mjs'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
