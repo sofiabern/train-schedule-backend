f904c1329fc54c89638431c82aaae793d3486d9b
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2026-02-08 18:57:06 +0200

    fix: configure CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
