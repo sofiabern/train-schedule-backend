920bdf17c4454f023615833311f336e0ddeecf8f
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2026-02-08 17:28:04 +0200

    chore: register modules and update bootstrap

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaService } from './prisma/prisma.service';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { StationModule } from './station/station.module';
+import { TrainModule } from './train/train.module';
+import { RouteModule } from './route/route.module';
+import { ScheduleModule } from './schedule/schedule.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [UserController],
-  providers: [PrismaService, UserService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    StationModule,
+    TrainModule,
+    RouteModule,
+    ScheduleModule,
+  ],
 })
 export class AppModule {}
